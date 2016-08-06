e228321866ff9281ce7a5be9cf0977d68bec2ab7
Martin Kloeppner <mkloeppner@me.com>
2016-08-06 12:38:43 +0200

    Fixed a issue with missing constructor parameter

--- MarsWeatherTests/MockFormattingService.swift
+++ MarsWeatherTests/MockFormattingService.swift
@@ -13,14 +13,19 @@ public struct MockFormattingService : FormattingService {
     
     var returnValue :String;
     
-    var dateReturnValue : NSDate?;
+    var dateReturnValue : NSDate? = nil
+    
+    public init(returnValue: String, dateReturnValue :NSDate? = nil) {
+        self.returnValue = returnValue
+        self.dateReturnValue = dateReturnValue
+    }
     
     public func string(fromTemperature double: Double) -> String {
-        return returnValue;
+        return returnValue
     }
     
     public func date(from value: String) -> NSDate? {
-        return dateReturnValue;
+        return dateReturnValue
     }
     
 }
