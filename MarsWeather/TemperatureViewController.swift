d34b79b532bf9d8a073c1f9c57c36fd279f00ffb
Martin Kloeppner <mkloeppner@me.com>
2016-08-06 10:17:01 +0200

    Added container for further display of temperature, pressure, wind, etc ...

---
+++ MarsWeather/TemperatureViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TemperatureViewController.swift
+//  MarsWeather
+//
+//  Created by Martin Klöppner on 06/08/16.
+//  Copyright © 2016 Martin Klöppner. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
